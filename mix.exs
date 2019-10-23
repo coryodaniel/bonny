857405b013c4ececb00dc0c3a12c043e161af393
Cory O'Daniel <cory@coryodaniel.com>
2019-10-23 14:23:36 -0700

    v 0.4.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Bonny.MixProject do
     [
       app: :bonny,
       description: description(),
-      version: "0.4.0-rc3",
+      version: "0.4.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
