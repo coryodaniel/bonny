fa57d23319b03de1d2f66bee9d08d967b5c05b76
Michael Ruoss <michael.ruoss@ufirstgroup.com>
2022-10-15 15:23:39 +0200

    add SetObservedGenerations pluggable step

---
+++ lib/bonny/pluggable/set_observed_generation.ex
@@ -0,0 +1,37 @@
+defmodule Bonny.Pluggable.SetObservedGeneration do
+  @moduledoc """
+  Copies the generation of the resource to the observedGeneration status field.
+  ## Options
+
+  `:observed_generation_key` - The key inside the resource status where the observed generation is stored.
+
+  ## Usage
+
+      step Bonny.Pluggable.SkipObservedGenerations,
+        observed_generation_key: ~w(internal observedGeneration)
+  """
+
+  @behaviour Pluggable
+  import Bonny.Axn
+
+  @impl true
+  def init(opts \\ []) do
+    Keyword.update(opts, :observed_generation_key, ["observedGeneration"], fn keys ->
+      [last_key | rest] = Enum.reverse(keys)
+
+      rest
+      |> Enum.map(fn
+        key when is_binary(key) -> Access.key(key, %{})
+        other -> other
+      end)
+      |> Enum.reverse([last_key])
+    end)
+  end
+
+  @impl true
+  def call(%Bonny.Axn{resource: resource} = axn, opts) do
+    observed_generation_key = Keyword.fetch!(opts, :observed_generation_key)
+    generation = resource["metadata"]["generation"]
+    update_status(axn, &put_in(&1, observed_generation_key, generation))
+  end
+end
